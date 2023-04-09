5d284b8ec7aff57314d5282366dbdf63b3c1f87b
semantic-release <semantic-release>
2023-04-09 11:56:41 -0400

    0.0.1-beta.2
    Automatically generated by python-semantic-release

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+<!--next-version-placeholder-->
+
+## v0.0.1-beta.2 (2023-04-09)
+
+
+## v0.0.1-beta.1 (2023-04-09)
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "0.0.0"
+__version__ = "0.0.1-beta.2"
 
 with open("README.md", "r") as fh:
     long_description = fh.read() or ""
