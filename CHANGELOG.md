41d08807d2bb5f84ecfc8e6e3ea1a07c5323dc1f
johnnygreco <jgreco.ai@gmail.com>
2023-04-16 12:48:06 -0400

    fixing release

--- CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# Changelog
-
-<!--next-version-placeholder-->
-
-## v0.0.1-beta.2 (2023-04-16)
-
-
-## v0.0.1-beta.1 (2023-04-16)
--- src/lmao/__init__.py
+++ src/lmao/__init__.py
@@ -2,4 +2,4 @@ from . import adapters, factory, prompters, tasks
 from .lm import clients, schemas
 from .lm.clients import AnthropicChatHistory, AnthropicClient, OpenAIChatHistory, OpenAIClient
 
-__version__ = "0.0.1-beta.2"
+__version__ = "0.0.1-beta.3"
