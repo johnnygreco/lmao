c236e5993a53e21717982c7e4a966ac668dcb603
semantic-release <semantic-release>
2023-04-16 11:46:05 -0400

    0.0.1-beta.8
    Automatically generated by python-semantic-release

--- src/lmao/__init__.py
+++ src/lmao/__init__.py
@@ -2,4 +2,4 @@ from . import adapters, factory, prompters, tasks
 from .lm import clients, schemas
 from .lm.clients import AnthropicChatHistory, AnthropicClient, OpenAIChatHistory, OpenAIClient
 
-__version__ = "0.0.1-beta.7"
+__version__ = "0.0.1-beta.8"
